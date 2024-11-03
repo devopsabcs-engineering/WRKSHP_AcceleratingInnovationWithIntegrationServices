1bde2df6976c13c8f870fb0159c1b370dd16c6c3
System Administrator <admin@mngenv019702.onmicrosoft.com>
2024-11-03 15:46:23 -0500

    test now works

--- testDeployment.ps1
+++ testDeployment.ps1
@@ -2,8 +2,13 @@ $apimGatewayUrl = "https://apim-ek001.azure-api.net"
 $apimSubscriptionKey = "e83871bbfa04480ba39bc35bc66c3148"
 $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 $headers = @{"Ocp-Apim-Subscription-Key" = $apimSubscriptionKey; "Content-Type" = "application/json" }
+$body = "{ `"date`" : `"$date`", `"id`" : `"1`", `"data`" : `"Sending data via APIM->Service Bus->Function->CosmosDB`" }"
+
+Write-Output "Sending data to APIM Gateway"
+Write-Output "CosmosDB data will show only one record with the current date: $date"
+Write-Output $body
 
 Invoke-WebRequest -Uri "$apimGatewayUrl/sb-operations/demo-queue" `
     -Headers $headers `
     -Method 'POST' `
-    -Body "{ `"date`" : `"$date`", `"id`" : `"1`", `"data`" : `"Sending data via APIM->Service Bus->Function->CosmosDB`" }"
+    -Body $body
